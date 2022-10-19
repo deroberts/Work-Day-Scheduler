f90ea25473031e9d2d9ebef3d65e0678dc648375
deroberts <s501656@gmail.com>
2022-10-19 13:20:54 -0500

    adding buttons with event listeners, as well as saving the input to local storage. But I'll have to do it for each button.

--- Develop/script.js
+++ Develop/script.js
@@ -1,8 +1,42 @@
-//make sure page is ready
-$(document).ready(function() {
 
-//display date at the top using moment
 var now = moment();
 $('#currentDay').text(now.format('dddd' + ", " + "MMMM Do"));
+
+
+
+
+  
+
+
+
+
+var saves = [];
+var addSave = (ev)=>{
+    ev.preventDefault();
+    var save = {
+        inpVal: document.getElementById('inpVal').value, 
+    // console logging to see if this gd button works. Okay it does now. 
+    // I'll either have to repeat this process for each button or find a better way to do this.
+}
+saves.push(save);
+;
+
+localStorage.setItem("WorkDayPlans", JSON.stringify(saves) );
+}
+
+//need the document to load
+document.addEventListener('DOMContentLoaded', ()=> {
+
+//there is probably a less tedious way to do this.    
+document.getElementById('button-addon').addEventListener('click', addSave);
+document.getElementById('button-addon1').addEventListener('click', addSave);
+document.getElementById('button-addon2').addEventListener('click', addSave);
+document.getElementById('button-addon3').addEventListener('click', addSave);
+document.getElementById('button-addon4').addEventListener('click', addSave);
+document.getElementById('button-addon5').addEventListener('click', addSave);
+document.getElementById('button-addon6').addEventListener('click', addSave);
+document.getElementById('button-addon7').addEventListener('click', addSave);
+document.getElementById('button-addon8').addEventListener('click', addSave);
+
 
 });
